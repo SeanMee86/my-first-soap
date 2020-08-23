cda67260148532ebd95818822e4013886ded92d1
SeanMee86 <sean.mee86@yahoo.com>
2020-08-23 06:11:07 -0700

    init commit

---
+++ src/main/java/com/seanmeedev/myfirstsoap/MyFirstSoapApplication.java
@@ -0,0 +1,13 @@
+package com.seanmeedev.myfirstsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seanmeedev/myfirstsoap/MyFirstSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanmeedev.myfirstsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
